263d09cd7aed40069fabd85247e7bab47af27d2b
Ethan <ekellogg2020@gmail.com>
2023-11-04 20:56:10 -0700

    new tutorial map

--- Assets/Scripts/TutorialScripts/TutorialManager.cs
+++ Assets/Scripts/TutorialScripts/TutorialManager.cs
@@ -42,7 +42,7 @@ public class TutorialManager : MonoBehaviour
     void Start()
     {
         GameManager.instance.pizzaAmount = 0;
-        
+        state = TutorialState.Joystick;
     }
 
     // Update is called once per frame
@@ -53,56 +53,6 @@ public class TutorialManager : MonoBehaviour
         {
             i.SetActive(true);
         }
-        switch (state)
-        {
-            case TutorialState.Joystick:
-                if (player.GetComponent<Rigidbody2D>().velocity.sqrMagnitude > 1)
-                    state++;
-                tutorialText.text = "Drag the joystick to move";
-                break;
-            case TutorialState.Boost:
-                if (player.boostPressed && player.boostMeter > 0)
-                    state++;
-                tutorialText.text = "Crash into an obstacle to build boost. You can press the boost button to temporarily speed up.";
-                break;
-            case TutorialState.Drift:
-                if (player.isDrifting)
-                {
-                    GameManager.instance.pizzaAmount = 0;
-                    GameCanvasManager.instance.UpdatePizzaAmount(GameManager.instance.pizzaAmount);
-                    state++;
-                }
-                tutorialText.text = "Try building up speed then press the drift button when you do turns on this road. It helps maintain your momentum and increases your boost.";
-                break;
-            case TutorialState.Pizza:
-
-                if (GameManager.instance.pizzaAmount > 0)
-                {
-                    state++;
-                    
-                }
-                tutorialText.text = "Get Pizza from the Pizzeria. You can use the small joystick on the right to throw pizzas.";
-                break;
-            case TutorialState.Zombie:
-                if (!zombieInScene.activeInHierarchy) // zombie was killed
-                {
-                    state++;
-                    GameManager.instance.ResetDeliveryTime();
-                }
-                tutorialText.text = "Up ahead is a zombie. It really likes eating pizzas, either from your bike or from the ground. Try to kill it by crashing into it with high speed.";
-                break;
-            case TutorialState.Delivery:
-                if (GameManager.instance.money > 0)
-                {
-                    // go to next scene ig
-                    Debug.Log("tutorial done");
-                    PlayerPrefs.SetInt("tutorial", 1);
-                    SceneManager.LoadScene(1); // 1 means game scene
-                }
-                tutorialText.text = "The green arrow points to the delivery zone. Deliver the pizza by either throwing it or entering the zone. Make deliveries within the time limit to maintian reputation. No reputation leads to a loss.";
-                break;
-        }
-
         //based on tutorial text, set dimension
 
         if (GameManager.instance.DeliveryTimeRatio() < 0.2f)
